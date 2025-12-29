38f087e1d60555072a1db83e2cfeb028a8d90709
Rau1-l <raulbeast.100@gmail.com>
2025-12-29 21:36:41 +0400

    classwork_2

---
+++ src/classwork_2/Classwork_2.java
@@ -0,0 +1,68 @@
+package classwork_2;
+
+import java.util.Iterator;
+
+public class Classwork_2 {
+	public static void main(String[] args) {
+   //1    
+    int age = 17;
+    if (age >= 18) {
+    System.out.println("+");
+    }else {
+    System.out.println("-");	
+    }
+    //2
+    //3
+  
+    int mark = 4;
+    if (mark > 4) {
+    System.out.println("Great !");
+    } else if (mark < 5) {
+    System.out.println("Normal!");
+    }else {
+    System.out.println("Bad !");	
+    }
+    //4
+    int temp = 26;
+    if (temp <= 26) {
+    System.out.println("SUUUUUUN");
+    }else {
+    System.out.println("NONE");
+    }
+    //5
+    //6
+    int marka = 5;
+    if (marka == 5) {
+    System.out.println("THE BEST ANSWER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }else {
+    System.out.println("BAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");	
+    }
+    //7
+    // 8
+    // 9
+    // 10
+    int i = 5;
+    if (i % 2 == 0) {
+    System.out.println("Чётное");
+    }else {
+    System.out.println("Нечётное");
+    }
+   
+    //11
+    String pol = "paren";
+    if (pol == "paren") {
+    System.out.println("TI PAREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");		
+	} else {
+    System.out.println("TI YA XZ KTO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	} 
+    //12
+    int point = 90;
+    if (point >= 90) {
+	System.out.println("GGGGGGGGGGGGGGGGGGGGGGGGRRRRRRRRRRRRRRRRREEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAATTTTTTTTTTTTTTTT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");	
+	}else {
+    System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADDDDDDDDDDDDDDDDDDDDDD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    //13 
+    //14
+	}
+	}
+	}
--- src/homework_1/Homework1.java
+++ src/homework_1/Homework1.java
@@ -1,7 +1,40 @@
-package homework_1;
+      package homework_1;
 
-public class Homework1 {
-
-	
-}
-
+      public class Homework1;
+	  public static void main(String[] args) {
+      //1
+	  for (int i = 0; i < 10; i++) {
+	  System.out.println("I like Java !");
+	  }
+      //2
+	  for (int i = 1; i <= 20; i++) {
+      System.out.println(i);
+      //3
+      for (int i = 10; i<= 1; i--) {
+      System.out.println(i);
+      //4
+      
+      //5
+      for (;;) {
+      System.out.println("Я люблю программировать!");
+      //6
+      
+      //7
+      String[] fruits = {"яблоко", "банан", "груша", "апельсин", "киви"};
+      System.out.println(fruits[fruits.length - 1]);
+      //8
+      int[] fruit = {1, 0, 1, 0, 1, 1, 1, 1, 0};
+      for (int ii= 0; i < fruits.length; ii++) {
+      System.out.println("Fruit " + (fruit[ii] == 0 ? "not ready" : "ready")); 
+      //9
+      
+      int number = 1;
+      do {
+      System.out.println(number++);  
+      }
+      while (number <= 10);
+      //10
+      
+      }
+      }
+      }
