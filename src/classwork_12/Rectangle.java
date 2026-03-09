eb102a2981d5f93c1bceb2354e25c7f1f8e19da9
Rau1-l <raulbeast.100@gmail.com>
2026-03-09 17:48:54 +0400

    classwork_12

--- src/classwork_12/Rectangle.java
+++ src/classwork_12/Rectangle.java
@@ -1,5 +1,14 @@
 package classwork_12;
 
-public class Rectangle {
+public class Rectangle implements Resizable {
+	double width = 10;
+	double height = 5;
 
+	@Override
+	public void resize(double factor) {
+		System.out.println(width * factor);
+		System.out.println(height * factor);
+		System.out.println(width % factor);
+		System.out.println(height % factor);
+	}
 }
--- src/classwork_13/Classwork_13.java
+++ src/classwork_13/Classwork_13.java
@@ -4,24 +4,34 @@ import java.util.Scanner;
 
 public class Classwork_13 {
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
+		System.out.println("-------------------------1------------------------");
+		try (Scanner sc = new Scanner(System.in)) {
 
-		System.out.println("Write 2 numbers");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+			System.out.println("Write 2 numbers");
+			int a = sc.nextInt();
+			int b = sc.nextInt();
 
-		System.out.println(a + b);
-		System.out.println(a - b);
-		System.out.println(a *b);
+			System.out.println(a + b);
+			System.out.println(a - b);
+			System.out.println(a * b);
 
-			if(b==0)
-			{
-			System.out.println("You cant decide by zero");
-		}else
-			{
+			if (b == 0) {
+				System.out.println("You cant decide by zero");
+			} else {
 				System.out.println("You can !");
 			}
+		}
+
+		System.out.println("------------------2-------------------");
+		System.out.println("Write number with six simvols !");
+	
+		Object sr;
+		int number = sr.Int();
+		
+		String[] number1 = sr.split("");
+		System.out.println(number1[0]);
+		
+		
 	}
 
 }
