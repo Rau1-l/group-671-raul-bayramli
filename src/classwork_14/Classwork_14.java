38836b4d0831009daef9c0c81aacf446015388a4
Rau1-l <raulbeast.100@gmail.com>
2026-03-16 21:36:52 +0400

    Classwork_14

---
+++ src/classwork_14/Classwork_14.java
@@ -0,0 +1,19 @@
+package classwork_14;
+
+import java.util.Random;
+
+public class Classwork_14 {
+	public static void main(String[] args) {
+		Math.random();
+		Random random = new Random();
+		int[] arr = new int[10];
+		for (int i = 0; i < arr.length;) {
+			arr[i] = random.nextInt(100);
+
+			int sum = 0;
+			for (int i = 0; i < arr.length;) {
+				sum = sum + arr[1];,
+			}
+		}
+	}
+}
